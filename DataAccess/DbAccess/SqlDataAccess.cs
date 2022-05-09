253e20fe997aa25edb023fca11b4d20728dc04ad
Daniel Bixler <danielbixler@yahoo.com>
2022-05-09 19:37:32 -0400

    Added Interface

--- DataAccess/DbAccess/SqlDataAccess.cs
+++ DataAccess/DbAccess/SqlDataAccess.cs
@@ -10,23 +10,23 @@ using System.Data;
 
 namespace DataAccess.DbAccess
 {
-    public class SqlDataAccess
+    public class SqlDataAccess : ISqlDataAccess
     {
         private readonly IConfiguration _config;
 
         public SqlDataAccess(IConfiguration config)
         {
-           _config = config;
+            _config = config;
         }
 
         public async Task<IEnumerable<T>> LoadData<T, U>(
-            string storedProcedure, 
-            U parameters, 
+            string storedProcedure,
+            U parameters,
             string connectionId = "Default")
         {
             using IDbConnection connection = new SqlConnection(_config.GetConnectionString(connectionId));
 
-            return await connection.QueryAsync<T>(storedProcedure, parameters, 
+            return await connection.QueryAsync<T>(storedProcedure, parameters,
                 commandType: CommandType.StoredProcedure);
         }
 
@@ -37,7 +37,7 @@ namespace DataAccess.DbAccess
         {
             using IDbConnection connection = new SqlConnection(_config.GetConnectionString(connectionId));
 
-            await connection.ExecuteAsync(storedProcedure, parameters, 
+            await connection.ExecuteAsync(storedProcedure, parameters,
                 commandType: CommandType.StoredProcedure);
         }
     }
